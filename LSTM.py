77915c676710cf10e665aff29d2fbc4af5fd017e
Hb.ZHANG <40030084+hbzhinsa@users.noreply.github.com>
2021-07-21 21:08:19 +0200

    LSTM test

---
+++ LSTM.py
@@ -0,0 +1,129 @@
+#%%
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+plt.style.use('dark_background')
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, GlobalMaxPooling1D, SpatialDropout1D
+
+# %%
+df_train = pd.read_csv('train.tsv', sep='\t')
+print('train set: {0}'.format(df_train.shape))
+df_train.head(10)
+# %%
+df_test = pd.read_csv('test.tsv', sep='\t')
+print('test set: {0}'.format(df_test.shape))
+df_test.head(10)
+# %%
+replace_list = {r"i'm": 'i am',
+                r"'re": ' are',
+                r"let’s": 'let us',
+                r"'s":  ' is',
+                r"'ve": ' have',
+                r"can't": 'can not',
+                r"cannot": 'can not',
+                r"shan’t": 'shall not',
+                r"n't": ' not',
+                r"'d": ' would',
+                r"'ll": ' will',
+                r"'scuse": 'excuse',
+                ',': ' ,',
+                '.': ' .',
+                '!': ' !',
+                '?': ' ?',
+                '\s+': ' '}
+def clean_text(text):
+    text = text.lower()
+    for s in replace_list:
+        text = text.replace(s, replace_list[s])
+    text = ' '.join(text.split())
+    return text
+# %%
+X_train = df_train['Phrase'].apply(lambda p: clean_text(p))
+# %%
+phrase_len = X_train.apply(lambda p: len(p.split(' ')))
+max_phrase_len = phrase_len.max()
+print('max phrase len: {0}'.format(max_phrase_len))
+plt.figure(figsize = (10, 8))
+plt.hist(phrase_len, alpha = 0.2, density = True)
+plt.xlabel('phrase len')
+plt.ylabel('probability')
+plt.grid(alpha = 0.25)
+# %%
+y_train = df_train['Sentiment']
+# %%
+max_words = 8192
+tokenizer = Tokenizer(
+    num_words = max_words,
+    filters = '"#$%&()*+-/:;<=>@[\]^_`{|}~'
+)
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_train = pad_sequences(X_train, maxlen = max_phrase_len)
+y_train = to_categorical(y_train)
+# %%
+batch_size = 512
+epochs = 8
+# %%
+model_lstm = Sequential()
+model_lstm.add(Embedding(input_dim = max_words, output_dim = 256, input_length = max_phrase_len))
+model_lstm.add(SpatialDropout1D(0.3))
+model_lstm.add(LSTM(256, dropout = 0.3, recurrent_dropout = 0.3))
+model_lstm.add(Dense(256, activation = 'relu'))
+model_lstm.add(Dropout(0.3))
+model_lstm.add(Dense(5, activation = 'softmax'))
+model_lstm.compile(
+    loss='categorical_crossentropy',
+    optimizer='Adam',
+    metrics=['accuracy']
+)
+
+# %%
+history = model_lstm.fit(
+    X_train,
+    y_train,
+    validation_split = 0.1,
+    epochs = 8,
+    batch_size = 512
+)
+#%%
+plt.clf()
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'g', label='Training loss')
+plt.plot(epochs, val_loss, 'y', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+#%%
+plt.clf()
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+plt.plot(epochs, acc, 'g', label='Training acc')
+plt.plot(epochs, val_acc, 'y', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+#%%
+X_test = tokenizer.texts_to_sequences(['this is a terrible thing'])
+x_test = pad_sequences(X_test, maxlen = max_phrase_len)
+y_test=model_lstm.predict(x_test)
+result  = np.argmax(y_test, axis=1)
+print('The sentiment predicted', result+1)
+
+X_test = tokenizer.texts_to_sequences(['this is good thing'])
+x_test = pad_sequences(X_test, maxlen = max_phrase_len)
+y_test=model_lstm.predict(x_test)
+result  = np.argmax(y_test, axis=1)
+print('The sentiment predicted', result+1)
+# %%
